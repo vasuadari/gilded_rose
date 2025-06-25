7a4e3b898b344ba857ab415b3f4020a09b730cf6
Vasu Adari <vasuakeel@gmail.com>
2025-06-25 11:06:08 +0530

    remove duplicate ruby directory

--- ruby/gilded_rose/lib/gilded_rose/item.rb
+++
@@ -1,9 +0,0 @@
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-end
--- ruby/gilded_rose/lib/gilded_rose/quality_manager.rb
+++
@@ -1,9 +0,0 @@
-class GildedRose
-  module QualityManager
-    def self.reduce_quality(item, quality_change)
-      new_quality = item.quality + quality_change
-
-      item.quality = [[new_quality, MIN_QUALITY].max, MAX_QUALITY].min
-    end
-  end
-end
